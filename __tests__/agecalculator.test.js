ef45dabbdd12fa9f4f9c4f2809e9dc6de3a8eeb7
Alex Bertotto <ajb5206@gmail.com>
2021-07-31 14:25:21 -0700

    add passing test for Venus age calculator

--- __tests__/agecalculator.test.js
+++ __tests__/agecalculator.test.js
@@ -10,5 +10,10 @@ describe('AgeCalculator', () => {
 	test('should correctly calculate Mercury age', () => {
 		const mercuryAge = new AgeCalculator(45);
 		expect(mercuryAge.calculateMercuryAge()).toEqual(187.5);
+	});
+
+	test('should correctly calculate Venus age', () => {
+		const venusAge = new AgeCalculator(45);
+		expect(venusAge.calculateVenusAge()).toEqual(72);
 	})
 });
--- src/agecalculator.js
+++ src/agecalculator.js
@@ -5,3 +5,7 @@ export function AgeCalculator(age){
 AgeCalculator.prototype.calculateMercuryAge = function() {
 	return (this.age*365)/(0.24*365);
 };
+
+AgeCalculator.prototype.calculateVenusAge = function() {
+	return parseInt(((this.age*365)/(0.62*365)).toFixed(1), 10);
+};
