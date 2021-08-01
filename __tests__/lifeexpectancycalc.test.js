499279732f41a290aa4df6ab0c45a0dae1095e7c
Alex Bertotto <ajb5206@gmail.com>
2021-08-01 00:09:14 -0700

    passing test on lifeexpectancy.js

--- __tests__/lifeexpectancycalc.test.js
+++ __tests__/lifeexpectancycalc.test.js
@@ -3,7 +3,7 @@ import LifeExpectancyCalc from './../src/lifeexpectancy.js';
 describe ('LifeExpectancyCalc', () => {
 
 	test('should properly create a life expectancy calc object', () => {
-		const lifeExpectancyCalc = new LifeExpectancyCalc(5);
-		expect(lifeExpectancyCalc.calculate()).toEqual(25);
+		const lifeExpectancyCalc = new LifeExpectancyCalc(80);
+		expect(lifeExpectancyCalc.calculate()).toEqual(0);
 	});
 });
