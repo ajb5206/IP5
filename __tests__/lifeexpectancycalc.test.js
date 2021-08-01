72edaf924d4e759a966699e08a2326093ee2415f
Alex Bertotto <ajb5206@gmail.com>
2021-07-31 23:39:15 -0700

    add files after passing test 1 for lifeexpectancy.js

--- __tests__/lifeexpectancycalc.test.js
+++ __tests__/lifeexpectancycalc.test.js
@@ -2,8 +2,8 @@ import LifeExpectancyCalc from './../src/lifeexpectancy.js';
 
 describe ('LifeExpectancyCalc', () => {
 
-	test('should properly create a  life expectancy calc object', () => {
+	test('should properly create a life expectancy calc object', () => {
 		const lifeExpectancyCalc = new LifeExpectancyCalc(5);
-		expect(calculate(timeleft)).toEqual(5);
+		expect(lifeExpectancyCalc.calculate()).toEqual(5);
 	});
 });
--- src/lifeexpectancy.js
+++ src/lifeexpectancy.js
@@ -1,6 +1,9 @@
 export default class LifeExpectancyCalc {
+	constructor(timeLeft) {
+		this.timeLeft = timeLeft;
+	}
 
 	calculate() {
-
+		return this.timeLeft = 5;
 	}
 }
